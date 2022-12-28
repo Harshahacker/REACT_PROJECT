e19d6fbfa1f15a22b0d68540b1bc88a26263748f
Harshahacker <102757031+Harshahacker@users.noreply.github.com>
2022-12-28 14:55:40 +0530

    Add files via upload
    REACT SIMPLE PROJECT

---
+++ src/TaskList.js
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function TaskList({
+  todos,
+  onChangeTodo,
+  onDeleteTodo
+}) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <Task
+            todo={todo}
+            onChange={onChangeTodo}
+            onDelete={onDeleteTodo}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Task({ todo, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false);
+  let todoContent;
+  if (isEditing) {
+    todoContent = (
+      <>
+        <input
+          value={todo.title}
+          onChange={e => {
+            onChange({
+              ...todo,
+              title: e.target.value
+            });
+          }} />
+        <button className='btn btn-warning m-2' onClick={() => setIsEditing(false)}>
+          Save
+        </button>
+      </>
+    );
+  } else {
+    todoContent = (
+      <>
+        {todo.title}
+        <button className='btn btn-secondary m-2' onClick={() => setIsEditing(true)}>
+          Edit
+        </button>
+      </>
+    );
+  }
+  return (
+    <label>
+      <input
+        type="checkbox"
+        checked={todo.done}
+        onChange={e => {
+          onChange({
+            ...todo,
+            done: e.target.checked
+          });
+        }}
+      />
+      {todoContent}
+      <button className='btn btn-primary' onClick={() => onDelete(todo.id)}>
+        Delete
+      </button>
+    </label>
+  );
+}
